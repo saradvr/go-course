52027103db568340ceb038a990c06fab00f1e23a
Sara Del Valle Restrepo <saradvr1608@gmail.com>
2021-06-06 17:55:36 -0500

    migration to Ubuntu

---
+++ 3_funciones/src/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var nombre string
+	// var apellido string
+	// var pais string
+
+	var nombre, apellido, pais string
+
+	fmt.Println(nombre, apellido, pais)
+}
