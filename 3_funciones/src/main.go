1341ea73c503a61eb73e577ac1c6e17ed76547a8
Sara Del Valle Restrepo <saradvr1608@gmail.com>
2021-06-06 21:07:39 -0500

    Lectura valores

--- 3_funciones/src/main.go
+++ 3_funciones/src/main.go
@@ -1,13 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	// var nombre string
-	// var apellido string
-	// var pais string
 
-	var nombre, apellido, pais string
-
-	fmt.Println(nombre, apellido, pais)
 }
