ee463280997a9e08f13e019ee67f8f94dd93b149
Sara Del Valle Restrepo <saradvr1608@gmail.com>
2021-06-06 21:39:12 -0500

    arreglos

---
+++ 6_Arreglos/src/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var numeros [5]int // el 5 es la longitud y no recibe ningún tipo diferente
+
+	// numeros[0] = 100
+	// numeros[4] = 300
+
+	// Inicializar el arreglo con valores
+	// numeros := [5]int{100, 200, 300, 400, 500}
+	// numeros := [...]int{100, 200, 300, 400, 500} //No se define la cantidad de elementos
+
+	// fmt.Println(numeros[4])
+
+	// Arreglos de String
+	// monedas := [...]string{"Peso Mexicano", "Dólar", "Euro"} //Quedan automáticamente en esos índices
+	// monedas := [...]string{0: "Peso Mexicano", 1: "Dólar", 2: "Euro"} //Queda en orden
+	monedas := [...]string{0: "Peso Mexicano", 2: "Dólar", 1: "Euro"} //Puedo cambiar el orden de los índices, si no pongo un índice le pone un string vacío. Si quiero varios indices vacíos, asigno un valor al último índice que quiero tener.
+
+	subMonedas := monedas[0:2]
+
+	fmt.Println(monedas, subMonedas)
+}
