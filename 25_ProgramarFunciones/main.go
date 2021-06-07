c7aafc5f9e44a44a1db3f09596dc3622e5843bd0
Sara Del Valle Restrepo <saradvr1608@gmail.com>
2021-06-07 15:14:28 -0500

    programar funciones y leer archivos txt

--- 25_ProgramarFunciones/main.go
+++ 25_ProgramarFunciones/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func funcion1() {
 	fmt.Println("Hola, soy funcion 1")
@@ -12,7 +15,34 @@ func funcion2() {
 
 func main() {
 
-	fmt.Println("Hola, estamos en la función Main")
-	defer funcion1() // se ejecuta cuando el bloque de la función main finalice. Sólo se ejecuta cuando la función que la llama termina
-	funcion2()
+	// EJEMPLO BÁSICO
+	// fmt.Println("Hola, estamos en la función Main")
+	// defer funcion1() // se ejecuta cuando el bloque de la función main finalice. Sólo se ejecuta cuando la función que la llama termina
+	// funcion2()
+
+	file, err := os.Open("example.txt")
+
+	if err != nil {
+		panic("No fue posible obtener el archivo")
+	}
+
+	defer func() {
+		fmt.Println("Cerramos el archivo!!")
+		file.Close()
+	}()
+
+	contenido := make([]byte, 254)
+
+	longitud, _ := file.Read(contenido) //el _ es err
+
+	fmt.Println(longitud)
+
+	fmt.Println(contenido[0:8])
+
+	contenidoArchivo := string(contenido[0:longitud]) //Convierte a texto todo el contenido
+
+	fmt.Println(contenido)
+
+	fmt.Println(contenidoArchivo)
+
 }
